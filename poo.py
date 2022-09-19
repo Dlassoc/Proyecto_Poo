6ae38f9a85d95e94653c2f92cfd7cc7a83527e7c
Daniel Lasso <dlassoc123@gmail.com>
2022-09-19 08:11:40 -0500

    Proyecto

---
+++ poo.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+import time
+from speech_recognition import Microphone, Recognizer, Audiofile, UnknownValueError, RequestErrror
+from gtts import gTTS
+from playsound import playsound
+import random 
+
+
+class Usuario:
+
+    def __init__(self, nombre, id):
+        self.nombre = nombre
+        self.id = id
+
+
+
+
+class Bot:
+
+    def validQR():
+        pass
+
+
+    def escribir():
+        pass
+
+
+    def enviar():
+        pass
+
+
+    def activarAsistente():
+        pass
+
+
+
